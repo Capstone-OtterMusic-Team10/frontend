b9f31498882c8fa8b0965ca610e40c49de8bc552
Pol <zhperon@gmail.com>
2025-05-09 00:33:37 -0700

    added utils file to work with backend api url added a jsx file to show playlists from spotify based on username

--- src/pages/SpotifyPage.jsx
+++ src/pages/SpotifyPage.jsx
@@ -1,11 +1,25 @@
-import MusicSubChat from "./MusicChat"
-
-
+import MusicSubChat from './MusicSubChat'
+import { useState, useEffect } from 'react'
+import { api } from '../utils'
+import SpotiPlaylistView from '../components/SpotiPlaylistView'
 const SpotifyPage =() =>{
+
+    const [playLists, setPlayLists] = useState([])
+    
+    const getPlayLists = async()=>{
+        const result = await api.get("get_spoti")
+        console.log(result)
+        setPlayLists(result.data.items)
+    }
+    useEffect(()=>{
+        getPlayLists() 
+    }, [])
     return (
         <>
             <div id="SpotifyPage">
-                <MusicSubChat/>
+                    {playLists && playLists.map((playList, idx)=>(
+                        <SpotiPlaylistView key={playList.id || idx} playlist={playList}/>
+                    ))}
             </div>
         </>
     )

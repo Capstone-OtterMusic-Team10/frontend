7d639737f492292eb2aef7e921e7e639629c5e1f
Pol <zhperon@gmail.com>
2025-06-02 23:50:47 -0700

    began About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,19 @@
 const About = () => {
     return(
         <>
-            <div>
-                Made by Henry Garkanian, Aryeh Freud, Pol Mejia, Eric Rodriguez, and Tony Suvorov
+            <div id="aboutPage">
+                <h1>About OtterMusic</h1>
+                <p>
+                OtterMusic is a user-friendly application that offers every user, 
+                regardless of their music skills, a creative freedom for music generation 
+                across different genres and purposes. It is a more carefree application that 
+                targets anyone who enjoys music, much like music applications we see today, 
+                relatively designed for recreational enjoyment. This application is 
+                important for music lovers in general. Inspired to be a creative enjoyment
+                of music appreciation, intertwined with our idea of including AI 
+                generative integrations to convince the user to use the application 
+                more out of their own curiosities.
+                </p>
             </div>
         </>
     )

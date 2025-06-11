8cfc4eb02bc6aefa486ba1870e2b13ad0aefd6a1
Pol <zhperon@gmail.com>
2025-06-11 15:04:25 -0700

    reformatting a test

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -8,10 +8,9 @@ describe('Home Page', () => {
     cy.contains('Welcome to Otter Music 🎵');
   });
 
-  it('loads successfully', () => {
-    cy.visit('http://localhost:5173'); 
-    cy.contains('Welcome to Otter Music 🎵').not();
-    cy.wait(2000)
-    cy.contains('Welcome to Otter Music 🎵');
+  it('contains all the required components and links (logged out)', () => {
+    cy.contains('Login')
+    cy.get('.footer-button').click() 
+    cy.url().should('include', '/about')
   });
 });

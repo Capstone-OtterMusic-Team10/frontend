26771ba71cfc493f993705aa2e30010359af4ae1
Pol <zhperon@gmail.com>
2025-07-13 15:55:34 -0700

    editing tests as the home page changed

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -3,7 +3,7 @@ describe('Home Page', () => {
     cy.visit('http://localhost:5173'); 
     cy.contains('Welcome to Otter Music 🎵').not();
     cy.contains('Text To Melodies');
-    cy.contains('Text To Playlist');
+    cy.contains('Workbench');
     cy.wait(2000)
     cy.contains('Welcome to Otter Music 🎵');
   });

b9de10eb9092e7977c83a04b4d6a173b87ffe565
Pol <zhperon@gmail.com>
2025-07-14 02:52:15 -0700

    wrote bad renaming script and had duplicate drum samples

--- rename.py
+++
@@ -1,8 +0,0 @@
-import os
-
-direct = "src/assets/music"
-with os.scandir(direct) as entries:
-    for entry in entries:
-        if entry.is_file():
-            new_name = entry.name.replace(" ", "_")
-            os.rename(f"src/assets/music/{entry.name}", new_name)

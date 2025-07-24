f2b560916879c839a52bcb39054bc4445aa41544
Pol <zhperon@gmail.com>
2025-07-23 23:33:05 -0700

    adjusted frontend navigation bar to change based on whether the user is logged in; we had two util files - merged stuff into one

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,8 @@ import { useNavigate } from 'react-router-dom';
 
 
 const Navbar = () =>{
-     const navigate = useNavigate();
+    const navigate = useNavigate();
+    const token = localStorage.getItem("token")
     return (
         <div id="navbar">
             <div id="leftNav">
@@ -14,12 +15,25 @@ const Navbar = () =>{
                 <button className="navButtons" onClick={() => navigate('/music-mixer')}>
                     Music Mixer
                 </button>
-                <button className="navButtons" id="logIn" onClick={() => navigate('/login')}>
-          Login
-        </button>
-                <button className="navButtons" id="signUp" onClick={() => navigate('/signup')}>
-                    Signup</button>
-            </div>
+                {!token ?
+                <>
+                    <button className="navButtons" id="logIn" onClick={() => navigate('/login')}>
+                        Login
+                    </button>
+                    <button className="navButtons" id="signUp" onClick={() => navigate('/signup')}>
+                        Signup
+                    </button>
+                </>
+                :
+                <button className="navButtons" id="logout" onClick={() => {
+                    localStorage.removeItem("token")
+                    navigate('/login')
+                    }}>
+                    Logout
+                </button>
+                }
+                </div>
+                
         </div>
     )
     
